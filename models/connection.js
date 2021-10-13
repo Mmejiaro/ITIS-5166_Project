5065c4de323d90ac37de13eb8c1122d78b18c72f
Michael Mejia <michaelmejia1171@gmail.com>
2021-10-13 17:50:02 -0400

    add model file and created 2 objects for connections

---
+++ models/connection.js
@@ -0,0 +1,22 @@
+const connections = [
+    {
+        id: '1',
+        title: 'Pizza Party',
+        img : 'img placeholder',
+        hostName: 'John Doe',
+        date: 'Thrusday, August 29, 2021',
+        time: '5:30pm - 6:30pm',
+        location: 'earth',
+        eventDetails: 'words',
+    },
+    {
+        id: '2',
+        title: 'Pool Party',
+        img : 'img placeholder',
+        hostName: 'Jane Doe',
+        date: 'Friday, October 15, 2021',
+        time: '2:30pm - 3:30pm',
+        location: 'earth',
+        eventDetails: 'words',
+    }
+]
