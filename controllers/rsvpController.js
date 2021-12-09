3c9d6a492daec6c72d19e0e82197684f87a355bb
Michael Mejia <michaelmejia1171@gmail.com>
2021-12-09 13:53:53 -0500

    initail controller created

---
+++ controllers/rsvpController.js
@@ -0,0 +1,7 @@
+const model = require('../models/rsvp');
+const Connection = require('../models/connection');
+const User = require('../models/user');
+
+exports.create = (req, res, next) => {
+    
+}
